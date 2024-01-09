df58bd7dae4bc1e4666f2be22051809dad574cad
José Quiñonez <Jquinonez@tsc.com.gt>
2024-01-09 08:14:11 -0600

    Se instalaron paquetes en todos los proyectos y referencias

--- SistemaInventario/Program.cs
+++ SistemaInventario/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SistemaInventario.Data;
+using SistemaInventario.AccesoDatos.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
